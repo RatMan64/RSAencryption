44d2329c18709fc2fe428291dd90fb3b69e35faa
RileyRat5x <35906066+RileyRat5x@users.noreply.github.com>
2022-04-03 22:03:50 -0700

    first commit setup for assignment

---
+++ RSA.py
@@ -0,0 +1,29 @@
+#Riley Barnes
+#cs427
+
+descKey = 65537
+
+testp = "9da5"
+testq = "b28b"
+testn = "6df25297"
+testt = "6df10268"
+testmssg = "hello, friend"
+messagehashtest = "1bcbce1"
+privatekeytest = "4a5a9c39"
+singedhashtest = "18e1eac9"
+uninvertedmsgtest = "1bcbce1"
+
+
+
+
+def rsasign():
+    return
+
+def rsaverify():
+
+    return
+
+
+if __name__ == '__main__':
+
+
